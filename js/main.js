d7a8af1af960d83a5cff0e7f3a837ef0f40dc5ec
kamura.f <y-music.like2@ezweb.ne.jp>
2025-12-11 15:43:13 +0900

    最終完成

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(function () {
   Inview（画面に表示されたタイミングで処理を実行）
   ===================================================*/
   $(window).scroll(function () {
-    $("").each(function () {
+    $(".content-left").each(function () {
 
       var scroll = $(window).scrollTop();
 
@@ -27,7 +27,54 @@ $(function () {
 
       if (scroll > target - windowHeight + $(this).outerHeight()) {
         // outerHeight()はpaddingを含めた高さを取得する
-        $(this).addClass("");
+        $(this).addClass("slide-right");
+      }
+    });
+  });
+  $(window).scroll(function () {
+    $(".content-right").each(function () {
+
+      var scroll = $(window).scrollTop();
+
+      var target = $(this).offset().top;
+
+      var windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("slide-left");
+      }
+    });
+  });
+
+    $(window).scroll(function () {
+    $(".nomal").each(function () {
+
+      var scroll = $(window).scrollTop();
+
+      var target = $(this).offset().top;
+
+      var windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("nomal-visible");
+      }
+    });
+  });
+
+    $(window).scroll(function () {
+    $(".revers").each(function () {
+
+      var scroll = $(window).scrollTop();
+
+      var target = $(this).offset().top;
+
+      var windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("revers-visible");
       }
     });
   });
